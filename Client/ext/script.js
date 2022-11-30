01964629388bcdeb5fb98f4cf49697c07a675300
wahyupambudi <wahyupambudi823@gmail.com>
2022-11-30 20:29:41 +0700

    create function input number 0-9

---
+++ Client/ext/script.js
@@ -0,0 +1,17 @@
+// script untuk membatasi inputan number
+const setNumber = (e) => {
+    // alternatif bisa pakai jquery masking
+    // For key or mouse events, this property indicates the specific key or button that was pressed
+    let key_code = (document) ? e.keyCode : e.which;
+
+    // jika kode ascii 48 - 57 (0-9)
+    if(key_code >= 48 && key_code <= 57 ) {
+        console.log(true);
+        return true;
+    } 
+    // jika kode ascii tidak 48 - 57 (0-9)
+    else {
+        console.log(false);
+        return false;
+    }
+}
