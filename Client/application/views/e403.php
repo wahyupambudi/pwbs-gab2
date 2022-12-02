60b6492179821030caf4a04f4a997a0bb1e08f00
wahyupambudi <wahyupambudi823@gmail.com>
2022-12-02 20:16:12 +0700

    custom forbiden 403

---
+++ Client/application/views/e403.php
@@ -0,0 +1,19 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>403</title>
+</head>
+
+<body>
+    <div align="center">
+        <h1>
+            Error 403 - Forbiden Access
+        </h1>
+        <p style="color:red; background-color:black; width: 350px; font-size: 25px;font-weight: bold; text-align:center;">MENGANTUK :)</p>
+    </div>
+</body>
+
+</html>
